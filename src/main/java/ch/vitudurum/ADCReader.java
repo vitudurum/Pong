92bdf45b9a0439f92a475cfb7912940b72ab2805
vitududurum <vitudurum@bluewin.ch>
2021-12-31 16:24:48 +0100

    mit Maven

--- src/main/java/ch/vitudurum/ADCReader.java
+++ src/main/java/ch/vitudurum/ADCReader.java
@@ -26,12 +26,14 @@ public class ADCReader {
                         "Failed to read configuration from address 0x" + String.format("%02x", TCA9534_REG_ADDR_CFG));
 
             System.out.println("IC2 Ready");
+            System.out.println("IC2 Write");
+            tca9534Dev.writeRegister(TCA9534_REG_ADDR_CFG, (byte) 0x84);
             System.out.println("Try reading");
             //byte currentState = (byte) tca9534Dev.readRegister(TCA9534_REG_ADDR_OUT_PORT);
-            byte zahl=tca9534Dev.readRegisterByte(TCA9534_REG_ADDR_OUT_PORT);
-            int zahl1=tca9534Dev.readRegisterWord(TCA9534_REG_ADDR_OUT_PORT);
+            int zahl=tca9534Dev.readRegisterByte(TCA9534_REG_ADDR_OUT_PORT);
+            //int zahl1=tca9534Dev.readRegisterWord(TCA9534_REG_ADDR_OUT_PORT);
             System.out.println("Value:"+zahl);
-            System.out.println("Value:"+zahl1);
+            //System.out.println("Value:"+zahl1);
             System.out.println("Finishing");
 /*
             if (config != 0x00) {
