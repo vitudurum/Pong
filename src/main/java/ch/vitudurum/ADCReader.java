732327f6d483f45de891d2bf9246e6386d891614
vitudurum <vitudurum@bluewin.ch>
2021-12-31 11:36:15 +0100

    with new Pi4J 2 Lib

--- src/main/java/ch/vitudurum/ADCReader.java
+++ src/main/java/ch/vitudurum/ADCReader.java
@@ -28,8 +28,10 @@ public class ADCReader {
             System.out.println("IC2 Ready");
             System.out.println("Try reading");
             //byte currentState = (byte) tca9534Dev.readRegister(TCA9534_REG_ADDR_OUT_PORT);
-            int zahl=tca9534Dev.readRegister(TCA9534_REG_ADDR_OUT_PORT);
+            byte zahl=tca9534Dev.readRegisterByte(TCA9534_REG_ADDR_OUT_PORT);
+            int zahl1=tca9534Dev.readRegisterWord(TCA9534_REG_ADDR_OUT_PORT);
             System.out.println("Value:"+zahl);
+            System.out.println("Value:"+zahl1);
             System.out.println("Finishing");
 /*
             if (config != 0x00) {
