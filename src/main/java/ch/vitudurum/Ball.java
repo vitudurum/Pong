68af1c1afb2ef4bb669b4af667cdeab987e9a18c
vitudurum <vitudurum@bluewin.ch>
2022-01-29 17:30:10 +0100

    Zwei Schalter

--- src/main/java/ch/vitudurum/Ball.java
+++ src/main/java/ch/vitudurum/Ball.java
@@ -123,7 +123,7 @@ public class Ball implements Runnable {
 	public int getADCValue(int id) {
 		if (id == 0)
 			return tca9534Dev.readRegister(TCA9534_REG_ADDR_OUT_PORT1);
-		if (id == 2)
+		if (id == 1)
 			return tca9534Dev.readRegister(TCA9534_REG_ADDR_OUT_PORT2);
 		return -1;
 	}
