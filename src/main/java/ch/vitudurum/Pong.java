292cdf15620e72b68005b7757159898367a92945
vitudurum <vitudurum@bluewin.ch>
2022-04-04 18:10:45 +0200

    New Resol.

--- src/main/java/ch/vitudurum/Pong.java
+++ src/main/java/ch/vitudurum/Pong.java
@@ -15,8 +15,8 @@ public class Pong extends JFrame  {
     //screen size variables.
     //static int gWidth = 1920;
     //static int gHeight = 1080;
-    static int gWidth = 1780;
-    static int gHeight = 1050;
+    static int gWidth = 1200;
+    static int gHeight = 1000;
     static int border_Up = 28;
     static int border_Down = gHeight - 30;
     static int border_Left = 0;
