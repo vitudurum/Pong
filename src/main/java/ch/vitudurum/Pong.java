e2bde30d6cdf7f6fa11acae6aaf8e0db3c6d71c2
vitudurum <vitudurum@bluewin.ch>
2022-04-09 13:35:10 +0200

    New Resol.

--- src/main/java/ch/vitudurum/Pong.java
+++ src/main/java/ch/vitudurum/Pong.java
@@ -15,14 +15,14 @@ public class Pong extends JFrame  {
     //screen size variables.
     //static int gWidth = 1920;
     //static int gHeight = 1080;
-    static int gWidth = 1700;
+    static int gWidth = 1740;
     static int gHeight = 1060;
     static int border_Up = 28;
     static int border_Down = gHeight - 30;
     static int border_Left = 0;
     static int border_Right = gWidth;
-    static int paddle_height = 170;
-    static int paddle_width = 25;
+    static int paddle_height = 180;
+    static int paddle_width = 30;
     Dimension screenSize = new Dimension(gWidth, gHeight);
     static ADCReader adc;
     Image dbImage;
